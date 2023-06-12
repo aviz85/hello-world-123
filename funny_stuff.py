c47ec880d66b877c74db3b623b2d866cdad39a07
aviz85 <avizmaeir@gmail.com>
2023-06-12 05:42:37 +0300

    Adding a funny python script

---
+++ funny_stuff.py
@@ -0,0 +1,11 @@
+import random
+
+def funny_stuff():
+    jokes = [
+        "What did the scientist say to the spaghteti? I want to pasta the tests.",
+        "Why did the computer show up at school? Because it was sure it could pass the test.",
+        "Why did the python go to the doctor? Because it can't stop shuedding."
+    ]
+    print(random.choice(jokes))
+
+funny_stuff()
